38130bca1defc13b7a41e61021c518c60f0415f9
鈴木湧 <suzukiyuu@PGD921511.local>
2023-01-31 12:57:30 +0900

    Update sum.test.js

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,10 +1,10 @@
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
+test('プログラムコメントの練習', () => {
 //テスト実行します。
 //あああ
   /*expect(sum(1, 2)).toBe(4);
-  //expect(sum(1, 2)).toBe(4);
+  //expect(sum(1, 2)).toBe(4); 
   ああああ
   あああああ*/
 });
