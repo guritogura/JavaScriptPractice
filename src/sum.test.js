f06ff57d7c28cbfb081e7f6fb7114fe843a5282e
鈴木湧 <suzukiyuu@PGD921511.local>
2023-02-07 12:58:11 +0900

    コンソールログの練習

--- src/sum.test.js
+++ src/sum.test.js
@@ -11,15 +11,24 @@ test('Constの練習', () => {
 
   });
 
-  test('Constの練習', () => {
-    let bookPrice = 3000;
-
-    let bookTitle = "ハリーポッター";
+test('Constの練習', () => {
+  let bookPrice = 3000;
+  let bookTitle = "ハリーポッター";
     
-    expect(bookTitle).toBe("ハリーポッター");
-    expect(bookPrice).toBe(3000);
+  expect(bookTitle).toBe("ハリーポッター");
+  expect(bookPrice).toBe(3000);
 
-    bookPrice = 300;
-    expect(bookPrice).toBe(300);
-    });
+  bookPrice = 300;
+  expect(bookPrice).toBe(300);
+});
   
+
+test('Consoleの練習', () => {
+  const total = 42+42;
+  console.log("total"); // => 1
+
+  const value = "値";
+  console.log(value); // => ReferenceError: x is no
+
+});
+
