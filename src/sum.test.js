85b644239d40ba689cd6eec1ca0664bba054e206
鈴木湧 <suzukiyuu@PGD921511.local>
2023-02-07 12:23:56 +0900

    Const練習

--- src/sum.test.js
+++ src/sum.test.js
@@ -5,6 +5,21 @@ const sum = require('./sum');
 test('Constの練習', () => {
   const bookTitle = "ハリーポッター"
   const bookPrice = 3000;
+
   expect(bookTitle).toBe("ハリーポッター");
   expect(bookPrice).toBe(3000);
+
   });
+
+  test('Constの練習', () => {
+    let bookPrice = 3000;
+
+    let bookTitle = "ハリーポッター";
+    
+    expect(bookTitle).toBe("ハリーポッター");
+    expect(bookPrice).toBe(3000);
+
+    bookPrice = 300;
+    expect(bookPrice).toBe(300);
+    });
+  
